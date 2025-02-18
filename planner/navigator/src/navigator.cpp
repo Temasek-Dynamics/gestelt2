f70888be1e7deaf17a21464c5d833a5ac63aee33
johntgz <johntgz@nus.edu.sg>
2025-02-18 18:13:06 +0800

    update coe

--- planner/navigator/src/navigator.cpp
+++ planner/navigator/src/navigator.cpp
@@ -1086,7 +1086,7 @@ bool Navigator::planCommlessMPC(const Eigen::Vector3d& goal_pos){
         fe_path_[lookahead_idx](0) - cur_pos_(0)
       );
 
-      cmd_yaw = std::atan2(dir_vec(0), dir_vec(1));
+      cmd_yaw = std::atan2(dir_vec(0), dir_vec(1))S;
     }
     else {
       Eigen::Vector2d dir_vec(
