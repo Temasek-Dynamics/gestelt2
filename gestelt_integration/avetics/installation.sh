1c98ea309910e9629bfd3f78ff347611339d4a9e
johntgz <john_tanguanzhong@hotmail.com>
2025-04-02 17:19:49 +0800

    update installation script

--- gestelt_integration/avetics/installation.sh
+++ gestelt_integration/avetics/installation.sh
@@ -14,7 +14,7 @@ sudo apt-get update && sudo apt-get install --no-install-recommends -y \
     libeigen3-dev \
     ros-$ROS_DISTRO-nav2-common \
     ros-$ROS_DISTRO-message-filters \
-    ros-$ROS_DISTRO-geographic-msgs \ 
-    ros-$ROS_DISTRO-geometry* \
-    ros-$ROS_DISTRO-tf2* \
     ros-$ROS_DISTRO-nav-2d-utils
+
+sudo apt-get install -y ros-$ROS_DISTRO-geometry*
+sudo apt-get install -y ros-$ROS_DISTRO-tf2*
