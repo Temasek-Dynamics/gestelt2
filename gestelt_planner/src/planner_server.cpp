4d04879a32cc24c93da88d6b76279e58a21e96d5
johntgz <john_tanguanzhong@hotmail.com>
2025-04-02 18:16:05 +0800

    removed logging of qualified names

--- gestelt_planner/src/planner_server.cpp
+++ gestelt_planner/src/planner_server.cpp
@@ -23,10 +23,10 @@ PlannerServer::PlannerServer(const rclcpp::NodeOptions &options)
 {
   RCLCPP_INFO(get_logger(), "Creating planner server");
   
-  RCLCPP_INFO(get_logger(), "get_fully_qualified_name: %s", 
-    get_fully_qualified_name());
-  RCLCPP_INFO(get_logger(), "get_namespace: %s", get_namespace());
-  RCLCPP_INFO(get_logger(), "get_name: %s", get_name());
+  // RCLCPP_INFO(get_logger(), "get_fully_qualified_name: %s", 
+  //   get_fully_qualified_name());
+  // RCLCPP_INFO(get_logger(), "get_namespace: %s", get_namespace());
+  // RCLCPP_INFO(get_logger(), "get_name: %s", get_name());
 
   // Declare this node's parameters
   declare_parameter("planner_plugins", default_ids_);
