2838bc22f4fc68417add4770f85d4b0e8d5a8e2c
johntgz <john_tanguanzhong@hotmail.com>
2025-05-28 17:12:09 +0800

    for test_send_cmds, pub to intermediate_cmd instead of px4 topic

--- gestelt_commander/gestelt_commander/test_send_cmds.py
+++ gestelt_commander/gestelt_commander/test_send_cmds.py
@@ -54,7 +54,7 @@ class OffboardControl(Node):
         )
 
         self.publisher_trajectory = self.create_publisher(
-            TrajectorySetpoint, '/fmu/in/trajectory_setpoint', qos_profile)
+            TrajectorySetpoint, '/intermediate_cmd', qos_profile)
         
         timer_freq = 10.0 # [Hz]
         self.timer = self.create_timer(1.0/timer_freq, self.cmdloop_callback)
