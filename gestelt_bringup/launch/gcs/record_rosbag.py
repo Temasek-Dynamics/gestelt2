e119c86672fca7430ad2d1917ce8588d632f4b9a
LennyLZ978 <linzhi263754@gmail.com>
2025-11-03 14:30:40 +0800

    Record multiple drones

--- gestelt_bringup/launch/gcs/record_rosbag.py
+++ gestelt_bringup/launch/gcs/record_rosbag.py
@@ -22,95 +22,143 @@ from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch_ros.actions import Node, PushRosNamespace, ComposableNodeContainer, SetParameter
 from launch_ros.descriptions import ComposableNode
 
-NUM_AGENTS = 1
+NUM_AGENTS = 2
 
 def generate_launch_description():
-    ns = "/d0"
+    # ns = "/d0"
 
+    bag_topics = []
+
+    for ns_index in range(NUM_AGENTS):
     # ROSBag 
-    bag_topics = [
-        # Pose
-        f"{ns}/odom",
-        f"{ns}/initialpose",
-        
-        # # Mapping
-        f"{ns}/local_map/bounds",
-        f"{ns}/local_occ_map/occ_map",
-        f"{ns}/local_occ_map/transition_event",
-        f"{ns}/occ_map",
-        f"{ns}/global_occ_map/occ_map",
-        f"{ns}/global_occ_map/transition_event",
-        f"{ns}/goal",
-        f"{ns}/goals",
+        sub_bag_topics = [
+            # Pose
+            f"/d{str(ns_index)}/odom",
+            f"/d{str(ns_index)}/initialpose",
 
-        # # Global Plan
-        f"{ns}/planner_server/transition_event",
-        f"{ns}/point_goal",
-        
-        # # Controller output
-        f"{ns}/sfc",
-        f"{ns}/received_global_plan",
-        f"{ns}/mpc_ref_path",
-        f"{ns}/mpc_curr_traj",
-        f"{ns}/mpc_traj",
-        f"{ns}/intmd_cmd",
-        f"{ns}/plan",
+            # # Mapping
+            f"/d{str(ns_index)}/local_map/bounds",
+            f"/d{str(ns_index)}/local_occ_map/occ_map",
+            f"/d{str(ns_index)}/local_occ_map/transition_event",
+            f"/d{str(ns_index)}/occ_map",
+            f"/d{str(ns_index)}/global_occ_map/occ_map",
+            f"/d{str(ns_index)}/global_occ_map/transition_event",
+            f"/d{str(ns_index)}/goal",
+            f"/d{str(ns_index)}/goals",
 
-        f"{ns}/controller_server/transition_event"
+            # # Global Plan
+            f"/d{str(ns_index)}/planner_server/transition_event",
+            f"/d{str(ns_index)}/point_goal",
 
-        # # Transformations
-        "/tf",
-        "/tf_static",
+            # # Controller output
+            f"/d{str(ns_index)}/sfc",
+            f"/d{str(ns_index)}/received_global_plan",
+            f"/d{str(ns_index)}/mpc_ref_path",
+            f"/d{str(ns_index)}/mpc_curr_traj",
+            f"/d{str(ns_index)}/mpc_traj",
+            f"/d{str(ns_index)}/intmd_cmd",
+            f"/d{str(ns_index)}/plan",
 
-        # # User commands
-        f"{ns}/uav_command",
-        "/global_uav_command",
+            f"/d{str(ns_index)}/controller_server/transition_event"
 
-        # PX4
-        f"{ns}/fmu/in/actuator_motors",
-        f"{ns}/fmu/in/actuator_servos",
-        f"{ns}/fmu/in/arming_check_reply",
-        f"{ns}/fmu/in/aux_global_position",
-        f"{ns}/fmu/in/config_control_setpoints",
-        f"{ns}/fmu/in/config_overrides_request",
-        f"{ns}/fmu/in/goto_setpoint",
-        f"{ns}/fmu/in/manual_control_input",
-        f"{ns}/fmu/in/message_format_request",
-        f"{ns}/fmu/in/mode_completed",
-        f"{ns}/fmu/in/obstacle_distance",
-        f"{ns}/fmu/in/offboard_control_mode",
-        f"{ns}/fmu/in/onboard_computer_status",
-        f"{ns}/fmu/in/register_ext_component_request",
-        f"{ns}/fmu/in/sensor_optical_flow",
-        f"{ns}/fmu/in/telemetry_status",
-        f"{ns}/fmu/in/trajectory_setpoint",
-        f"{ns}/fmu/in/unregister_ext_component",
-        f"{ns}/fmu/in/vehicle_attitude_setpoint",
-        f"{ns}/fmu/in/vehicle_command",
-        f"{ns}/fmu/in/vehicle_command_mode_executor",
-        f"{ns}/fmu/in/vehicle_mocap_odometry",
-        f"{ns}/fmu/in/vehicle_rates_setpoint",
-        f"{ns}/fmu/in/vehicle_thrust_setpoint",
-        f"{ns}/fmu/in/vehicle_torque_setpoint",
-        f"{ns}/fmu/in/vehicle_trajectory_bezier",
-        f"{ns}/fmu/in/vehicle_trajectory_waypoint",
-        f"{ns}/fmu/in/vehicle_visual_odometry",
-        f"{ns}/fmu/out/estimator_status_flags",
-        f"{ns}/fmu/out/failsafe_flags",
-        f"{ns}/fmu/out/manual_control_setpoint",
-        f"{ns}/fmu/out/position_setpoint_triplet",
-        f"{ns}/fmu/out/sensor_combined",
-        f"{ns}/fmu/out/timesync_status",
-        f"{ns}/fmu/out/vehicle_attitude",
-        f"{ns}/fmu/out/vehicle_command_ack",
-        f"{ns}/fmu/out/vehicle_control_mode",
-        f"{ns}/fmu/out/vehicle_global_position",
-        f"{ns}/fmu/out/vehicle_gps_position",
-        f"{ns}/fmu/out/vehicle_land_detected",
-        f"{ns}/fmu/out/vehicle_local_position",
-        f"{ns}/fmu/out/vehicle_odometry",
-        f"{ns}/fmu/out/vehicle_status",
+            # # Transformations
+            # "/tf",
+            # "/tf_static",
 
+            # # User commands
+            f"/d{str(ns_index)}/uav_command",
+            # "/global_uav_command",
+
+            # PX4
+            f"/d{str(ns_index)}/fmu/in/actuator_motors",
+            f"/d{str(ns_index)}/fmu/in/actuator_servos",
+            f"/d{str(ns_index)}/fmu/in/arming_check_reply",
+            f"/d{str(ns_index)}/fmu/in/aux_global_position",
+            f"/d{str(ns_index)}/fmu/in/config_control_setpoints",
+            f"/d{str(ns_index)}/fmu/in/config_overrides_request",
+            f"/d{str(ns_index)}/fmu/in/goto_setpoint",
+            f"/d{str(ns_index)}/fmu/in/manual_control_input",
+            f"/d{str(ns_index)}/fmu/in/message_format_request",
+            f"/d{str(ns_index)}/fmu/in/mode_completed",
+            f"/d{str(ns_index)}/fmu/in/obstacle_distance",
+            f"/d{str(ns_index)}/fmu/in/offboard_control_mode",
+            f"/d{str(ns_index)}/fmu/in/onboard_computer_status",
+            f"/d{str(ns_index)}/fmu/in/register_ext_component_request",
+            f"/d{str(ns_index)}/fmu/in/sensor_optical_flow",
+            f"/d{str(ns_index)}/fmu/in/telemetry_status",
+            f"/d{str(ns_index)}/fmu/in/trajectory_setpoint",
+            f"/d{str(ns_index)}/fmu/in/unregister_ext_component",
+            f"/d{str(ns_index)}/fmu/in/vehicle_attitude_setpoint",
+            f"/d{str(ns_index)}/fmu/in/vehicle_command",
+            f"/d{str(ns_index)}/fmu/in/vehicle_command_mode_executor",
+            f"/d{str(ns_index)}/fmu/in/vehicle_mocap_odometry",
+            f"/d{str(ns_index)}/fmu/in/vehicle_rates_setpoint",
+            f"/d{str(ns_index)}/fmu/in/vehicle_thrust_setpoint",
+            f"/d{str(ns_index)}/fmu/in/vehicle_torque_setpoint",
+            f"/d{str(ns_index)}/fmu/in/vehicle_trajectory_bezier",
+            f"/d{str(ns_index)}/fmu/in/vehicle_trajectory_waypoint",
+            f"/d{str(ns_index)}/fmu/in/vehicle_visual_odometry",
+            f"/d{str(ns_index)}/fmu/out/estimator_status_flags",
+            f"/d{str(ns_index)}/fmu/out/failsafe_flags",
+            f"/d{str(ns_index)}/fmu/out/manual_control_setpoint",
+            f"/d{str(ns_index)}/fmu/out/position_setpoint_triplet",
+            f"/d{str(ns_index)}/fmu/out/sensor_combined",
+            f"/d{str(ns_index)}/fmu/out/timesync_status",
+            f"/d{str(ns_index)}/fmu/out/vehicle_attitude",
+            f"/d{str(ns_index)}/fmu/out/vehicle_command_ack",
+            f"/d{str(ns_index)}/fmu/out/vehicle_control_mode",
+            f"/d{str(ns_index)}/fmu/out/vehicle_global_position",
+            f"/d{str(ns_index)}/fmu/out/vehicle_gps_position",
+            f"/d{str(ns_index)}/fmu/out/vehicle_land_detected",
+            f"/d{str(ns_index)}/fmu/out/vehicle_local_position",
+            f"/d{str(ns_index)}/fmu/out/vehicle_odometry",
+            f"/d{str(ns_index)}/fmu/out/vehicle_status",
+
+            # # # Others
+            # "/depth_camera",
+            # "/depth_camera/points"
+            # "/diagnostics",
+            # "/fake_map",
+            # "/fe_plan/viz",
+            # "/fe_plan/viz_array",
+            # "/fe_plan_req",
+            # "/fe_plan_req_array",
+            # "/initialpose",
+            # "/local_map/bounds",
+            # "/mpc/traj",
+            # "/occ_map/reset_map",
+            # "/odom",
+            # "/parameter_events",
+            # "/point_goal",
+
+            # "/rosout",
+
+            # f"/agent_id_text",
+            # f"/agent_id_text_array",
+            # f"/amcl_pose",
+            # f"/camera",
+            # f"/camera_info",
+            # f"/clicked_point",
+            # f"/clock",
+            f"/d{str(ns_index)}/cloud",
+            f"/d{str(ns_index)}/cloud_global",
+            f"/d{str(ns_index)}/agent_id_text",
+            f"/d{str(ns_index)}/agent_id_text_array",
+            f"/d{str(ns_index)}/amcl_pose",
+            f"/d{str(ns_index)}/bond",
+            f"/d{str(ns_index)}/fe_plan/viz",
+            f"/d{str(ns_index)}/fe_plan/viz_array",
+            f"/d{str(ns_index)}/fe_plan_req",
+            f"/d{str(ns_index)}/fe_plan_req_array",
+            f"/d{str(ns_index)}/reset_map",
+            f"/d{str(ns_index)}/uav_state",
+            f"/visbot_itof/point_cloud",
+        ]
+
+        for topic in sub_bag_topics:
+            bag_topics.append(topic)
+    
+    extra_topics = [
         # # Others
         "/depth_camera",
         "/depth_camera/points"
@@ -137,20 +185,19 @@ def generate_launch_description():
         f"/camera_info",
         f"/clicked_point",
         f"/clock",
-        f"/cloud",
-        f"/cloud_global",
-        f"{ns}/agent_id_text",
-        f"{ns}/agent_id_text_array",
-        f"{ns}/amcl_pose",
-        f"{ns}/bond",
-        f"{ns}/fe_plan/viz",
-        f"{ns}/fe_plan/viz_array",
-        f"{ns}/fe_plan_req",
-        f"{ns}/fe_plan_req_array",
-        f"{ns}/reset_map",
-        f"{ns}/uav_state",
-        f"/visbot_itof/point_cloud",
+
+        # # User commands
+        "/global_uav_command",
+
+        # Transformations
+        "/tf",
+        "/tf_static",
     ]
+
+    for topic in extra_topics:
+        bag_topics.append(topic)
+
+    
 
     bag_file = os.path.join(
         os.path.expanduser("~"), 'bag_files',
